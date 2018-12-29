74cad6d85f29876103a363f922326f7eb6126af5
李笑 <Wade2@sf-express.com>
2018-12-29 20:30:11 +0800

    删除功能完善

--- src/Listeners/JTreeListeners.java
+++ src/Listeners/JTreeListeners.java
@@ -4,20 +4,29 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-
+import java.io.File;
+import java.io.IOException;
 import javax.swing.JTree;
 import javax.swing.event.TreeModelEvent;
 import javax.swing.event.TreeModelListener;
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.DefaultTreeModel;
-import javax.swing.tree.MutableTreeNode;
-import javax.swing.tree.TreeModel;
 import javax.swing.tree.TreeNode;
 import javax.swing.tree.TreePath;
-
+import org.apache.commons.io.FileUtils;
 import Client.Editor;
 
 public class JTreeListeners implements TreeModelListener,ActionListener{
+	
+	private String workPath;
+	private String testCasePath;
+	private String templetePath;
+	
+	{
+		workPath = System.getProperty("user.dir") + File.separator; // 工作路径
+		testCasePath = workPath + "TestCases\\"; // 用例路径
+		templetePath = workPath + "Templete\\"; // 模板路径
+	}
 
 	@Override
 	public void treeNodesChanged(TreeModelEvent e) {
@@ -92,50 +101,114 @@ public class JTreeListeners implements TreeModelListener,ActionListener{
 				treeModel.insertNodeInto(newNode, root, root.getChildCount());
 				// 展开节点
 				tree.scrollPathToVisible(new TreePath(newNode.getPath()));
+				
+				// 新增模块文件夹				
+				File moduleFolder = new File(testCasePath,"新模块");
+				if (!moduleFolder.exists()) {
+					moduleFolder.mkdirs();
+				}
 			}
 			if (ae.getActionCommand().equals("新增用例")) {
 				DefaultMutableTreeNode newNode = new DefaultMutableTreeNode("新用例");
 				newNode.setAllowsChildren(false);
 				TreePath treepath = tree.getSelectionPath();
-				if (treepath != null && treepath.getPath().length <= 2) { // 层级不能大于2
+				if (treepath != null && treepath.getPath().length == 2) { // 层级等于2，说明为模块
 					// 获取选中的节点
 					DefaultMutableTreeNode selectionNode = (DefaultMutableTreeNode) treepath.getLastPathComponent();
-					if (selectionNode != null && selectionNode != treeModel.getRoot()) { // 不能为null或者根节点
-						treeModel.insertNodeInto(newNode, selectionNode, selectionNode.getChildCount());
-						tree.scrollPathToVisible(new TreePath(newNode.getPath()));
+					treeModel.insertNodeInto(newNode, selectionNode, selectionNode.getChildCount());
+					tree.scrollPathToVisible(new TreePath(newNode.getPath()));
+						
+					// 新建用例文件
+					File srcFile = new File (templetePath + "CaseTemplate.xls");
+					File destFile = new File (testCasePath + selectionNode.toString() + "\\新用例.xls");
+					try {
+						FileUtils.copyFile(srcFile, destFile);
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
 					}
+					return;
 				}
-				// 不符合要求
-				System.out.println("请指定模块！");		
+				// 其他情况
+				System.out.println("请指定模块！");
 			}
 			if (ae.getActionCommand().equals("删除")) {
 				TreePath treepath = tree.getSelectionPath();
 				if(treepath == null) { //没有选节点
 					System.out.println("请选择要删除的节点！");
 					return;
-				}
-				
+				}				
 				int level = treepath.getPath().length;
 				// 获取选中的节点
 				DefaultMutableTreeNode selectionNode = (DefaultMutableTreeNode) treepath.getLastPathComponent();
 				switch (level) {
 				case 1 :
 					System.out.println("确定删除所有模块和用例？");
-					selectionNode.removeAllChildren();
-					treeModel.reload(); // 重新加载树
+					if (this.clearAllModules()){
+						selectionNode.removeAllChildren();
+						treeModel.reload(); // 重新加载树
+						return;
+					}										
 					break;
 				case 2 :
-					System.out.println("确定删除该模块？");					
-					treeModel.removeNodeFromParent(selectionNode);
+					System.out.println("确定删除该模块？");
+					if(this.clearUniqueModule(selectionNode.toString())) {
+						treeModel.removeNodeFromParent(selectionNode); // 删除节点
+						return;
+					}					
 					break;
 			    case 3 :
 				    System.out.println("确定删除该用例？");
-				    treeModel.removeNodeFromParent(selectionNode);
+				    String moduleName = treepath.getPath()[1].toString();
+				    String caseName = selectionNode.toString();
+				    if (this.clearUniqueCase(moduleName,caseName)) {
+				    	treeModel.removeNodeFromParent(selectionNode); // 删除节点
+				    	return;
+				    }		
 				    break;
 			    }
-			}
-			
-			
+				System.out.println("删除失败，请稍后再试！");
+			}			
 		}
+		
+		private boolean clearAllModules() {
+			// 删除所有模块文件夹
+			File[] files = new File (testCasePath).listFiles();
+			for (File file : files) {
+				try {
+					FileUtils.deleteDirectory(file);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+					return false;
+				}
+			}
+			return true;
+		}
+		
+		private boolean clearUniqueModule(String moduleName) {
+			// 删除模块目录
+			try {
+				FileUtils.deleteDirectory(new File(testCasePath + moduleName));
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				return false;
+			}
+			return true;
+		}
+		
+		private boolean clearUniqueCase(String moduleName, String caseName) {
+			// 删除用例文件
+			String filePath = testCasePath + moduleName + File.separator + caseName + ".xls";
+			try {
+				new File(filePath).delete();
+			} catch (Exception e) {
+				return false;
+			}
+			return true;
+		}
+		
+		
 
 }
